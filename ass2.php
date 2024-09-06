88db9459a0dc6fb3ccec4e3fa843bc782004faa7
Nhem Day <aclonhemday@gmail.com>
2024-09-06 19:14:00 +0800

    first commit

---
+++ ass2.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Fullname:EARL LLOYD T.SAGA <br>";
+    echo "Address:Camanucan,Panaon mis. Occ <br>";
+    echo "Email Address:sagaearlloyd@gmail.com<br>";
+    echo "Parents:Ernie B. Saga <br>";
+    echo "Mesraem T. Saga <br>";
+    echo "social media:earlloyd <br>";
---
+++ assignment.php
@@ -0,0 +1,8 @@
+<?php
+    echo "name: Nhem day G. Aclo <br>";
+    echo "address: Baga, Panaon, Misamis Occidental <br>";
+    echo "age=19 <br>";
+    echo "contact number: 09674185880<br>";
+    echo "parents: Mila G. Aclo and Devie C. Aclo <br>";
+    echo "Social Media: Nhem Day G. aclo<br>";
+?>
